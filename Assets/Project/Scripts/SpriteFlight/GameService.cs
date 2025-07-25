f1237c0abf7dbc6d8ffab5faf0cfd45f54bafccb
Renier Gaytan <renier.gaytan@gmail.com>
2025-07-24 23:08:08 -0600

    dummy

---
+++ Assets/Project/Scripts/SpriteFlight/GameService.cs
@@ -0,0 +1,6 @@
+namespace SpriteFlight
+{
+    public static class GameService {
+        
+    }
+}
--- Assets/Project/Scripts/SpriteFlight/ScorePresenter.cs
+++ Assets/Project/Scripts/SpriteFlight/ScorePresenter.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Runtime.Serialization;
+using Codice.Client.Common;
+using UtilitiesLibrary;
+
 namespace SpriteFlight
 {
     public class ScorePresenter
@@ -5,7 +10,57 @@ namespace SpriteFlight
         ScoreModel scoreModel;
         ScoreView scoreView;
         private float timer;
-        
-        
+        private float interval = 1f;
+        TimeSpan _timeSpan;
+        FrequencyTimer frequencyTimer;
+
+
+        public ScorePresenter(ScoreModel model, ScoreView view)
+        {
+            scoreModel = model;
+            scoreView = view;
+            scoreView.SetPresenent(this);
+
+            //Timer
+            frequencyTimer = new FrequencyTimer(1);
+            frequencyTimer.OnTick += OnSecondPassed;
+            frequencyTimer.Start();
+
+
+            view.UpdateScoreDisplay(FormatTime(_timeSpan));
+        }
+
+        public void OnSecondPassed()
+        {
+            _timeSpan = _timeSpan.Add(TimeSpan.FromSeconds(1));
+            scoreView.UpdateScoreDisplay(FormatTime(_timeSpan));
+        }
+
+        public string FormatTime(TimeSpan ts)
+        {
+            return string.Format("{0:D2}:{0:D2}", ts.Minutes, ts.Seconds);
+        }
+
+        public void Reset()
+        {
+            frequencyTimer.Reset();
+            _timeSpan = TimeSpan.Zero;
+            scoreView.UpdateScoreDisplay(FormatTime(_timeSpan));
+        }
+        public void Stop()
+        {
+            frequencyTimer?.Stop();
+        }
+        public void Dispose()
+        {
+            frequencyTimer.OnTick -= OnSecondPassed;
+            frequencyTimer.Dispose();
+        }
+
+        private void OnDestroy()
+        {
+            Dispose();
+        }
+
     }
 }
